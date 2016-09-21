46bd97da4b45f55c7f3f050ce644d46c6979e858
AtlantisPhantom <kronos85@gmail.com>
2016-09-21 11:18:14 +0200

    Added 'prettyCase' (filter) and 'prettyCaseFunc' (value <- function)

--- js/modules/utils.js
+++ js/modules/utils.js
@@ -1,3 +1,17 @@
 
 angular.module('Utils', [])
-	.value('defaultUser', {"civility":"M.", "surname":"Steve", "name":"Martins Pires"});
+	.value('defaultUser', {"civility":"M.", "surname":"Steve", "name":"Martins Pires"})
+	.value('prettyCaseFunc', function( stringToPrettify) {
+		var _result = '';
+		var _words = stringToPrettify.split(' ');
+
+		for (var i = 0 ; i<= _words.length - 1; i++) {
+			var _word = _words[i].toLowerCase();
+			if('' != _word) {
+				var _newWord = _word[0].toUpperCase() + _word.substring(1);
+				if(0 != i) { _result += ' '};
+				_result += _newWord;
+			}
+		}
+		return _result;
+	});
