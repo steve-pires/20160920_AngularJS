d576a27938c519824f0110d4427f013cf57a9517
AtlantisPhantom <kronos85@gmail.com>
2016-09-21 11:22:34 +0200

    using value function in app.filter prettyCase !

--- js/spa.js
+++ js/spa.js
@@ -132,22 +132,9 @@ app.controller('CartCtrl', ['$scope', function ($scope) {
 
 
 ///// FILTRES
-app.filter('prettyCase', function() {
-	return function( stringToPrettify) {
-		var _result = '';
-		var _words = stringToPrettify.split(' ');
-
-		for (var i = 0 ; i<= _words.length - 1; i++) {
-			var _word = _words[i].toLowerCase();
-			if('' != _word) {
-				var _newWord = _word[0].toUpperCase() + _word.substring(1);
-				if(0 != i) { _result += ' '};
-				_result += _newWord;
-			}
-		}
-		return _result;
-	};
-});
+app.filter('prettyCase', ['prettyCaseFunc', function(prettyCaseFunc) { 
+	return prettyCaseFunc;
+}]);
 
 
 ///// JS FUNCTIONS
